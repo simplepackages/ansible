e466ad7a3d8fca7cab8e294e12bc999b46cf9eb8
Daniil Kirichenko <daniil@kirick.me>
2024-08-19 11:25:50 +0300

    Now building only ansible-core

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,13 @@
 
-FROM		alpine:3.17
-ARG			PYTHON_DIR='/usr/lib/python3.10/site-packages'
-ARG			ANSIBLE_COLLECTIONS_DIR=${PYTHON_DIR}'/ansible_collections'
-ARG			TMP_ANSIBLE_COLLECTIONS_DIR='/tmp/ansible_collections'
-RUN			apk add --no-cache ansible openssh sshpass rsync \
-			&& mkdir -p ${TMP_ANSIBLE_COLLECTIONS_DIR} \
-			&& mv ${ANSIBLE_COLLECTIONS_DIR}/ansible \
-			      ${TMP_ANSIBLE_COLLECTIONS_DIR}/ansible \
-			&& rm -rf ${ANSIBLE_COLLECTIONS_DIR}/* \
-			&& mv ${TMP_ANSIBLE_COLLECTIONS_DIR}/ansible \
-			      ${ANSIBLE_COLLECTIONS_DIR}/ansible \
-			&& rm -rf ${ANSIBLE_COLLECTIONS_DIR}/ansible/*/tests \
+ARG			ALPINE_VERSION
+FROM		alpine:${ALPINE_VERSION}
+ARG			ANSIBLE_VERSION
+ARG			PYTHON_DIR='/usr/lib/python3.*/site-packages'
+RUN			apk add --no-cache ansible-core=${ANSIBLE_VERSION}-r0 openssh sshpass rsync py3-pip \
 			&& rm -rf ${PYTHON_DIR}/ansible_test \
+			&& pip install --no-cache-dir --break-system-packages passlib \
+			&& apk del py3-pip \
 			&& rm -rf /var/cache/apk/* \
+			&& rm -rf /root/.cache/pip \
 			&& rm -rf /tmp/*
 ENTRYPOINT	[ "/usr/bin/ansible" ]
--- build.sh
+++ build.sh
@@ -1,24 +1,46 @@
-#!/bin/sh
+#!/bin/bash -e
 
-RESULT=$(docker run --rm --entrypoint /bin/sh alpine:3.17 -c 'apk add ansible-core >/dev/null 2>&1; ansible --version')
+ALPINE_RESULT=$(docker run --pull always --rm --entrypoint /bin/sh alpine:latest -c 'cat /etc/os-release | grep VERSION_ID')
+# echo 'ALPINE_RESULT = '$ALPINE_RESULT
+ALPINE_VERSION=$(echo $ALPINE_RESULT | cut -d '=' -f 2)
+# ALPINE_VERSION='3.20.2'
+echo 'ALPINE_VERSION = '$ALPINE_VERSION
+
+RESULT=$(docker run --rm --entrypoint /bin/sh alpine:${ALPINE_VERSION} -c '(apk update && apk add ansible-core) >/dev/null 2>&1 ; ansible --version')
+# echo 'RESULT = '$RESULT
 ANSIBLE_VERSION=$(echo $RESULT | grep 'ansible \[core' | cut -d ' ' -f 3 | cut -d ']' -f 1)
+# ANSIBLE_VERSION='2.17.0'
 echo 'ANSIBLE_VERSION = '$ANSIBLE_VERSION
 
-DOCKER_IMAGE='simplepackages/ansible'
+DOCKER_IMAGE='simplepackages/ansible-core'
 DOCKER_TAG=$ANSIBLE_VERSION
 
-docker manifest inspect $DOCKER_IMAGE:$DOCKER_TAG >/dev/null 2>&1
-if [ $? -ne 0 ]; then
-    docker buildx create --name multibuilder
+MANIFEST_RESULT=$(docker manifest inspect $DOCKER_IMAGE:$DOCKER_TAG 2>&1 || true)
+# echo 'MANIFEST_RESULT = '$MANIFEST_RESULT
+if [ -n "$(echo $MANIFEST_RESULT | grep 'no such manifest')" ]; then
+    echo 'Building '$DOCKER_IMAGE:$DOCKER_TAG'...'
+    echo
+
+    # docker build --build-arg "ALPINE_VERSION=$ALPINE_VERSION" \
+    #              --build-arg "ANSIBLE_VERSION=$ANSIBLE_VERSION" \
+    #              --tag $DOCKER_IMAGE:$DOCKER_TAG \
+    #              .
+
+    docker buildx create --name multibuilder || true
     docker buildx use multibuilder
     docker buildx build --push \
                         --platform linux/amd64,linux/arm64,linux/arm/v7 \
+                        --build-arg "ALPINE_VERSION=$ALPINE_VERSION" \
+                        --build-arg "ANSIBLE_VERSION=$ANSIBLE_VERSION" \
                         --tag $DOCKER_IMAGE:$DOCKER_TAG \
                         --tag $DOCKER_IMAGE:latest \
                         .
-else
+elif [ -n "$(echo $MANIFEST_RESULT | grep '"digest": "sha256:')" ]; then
     echo 'Image '$DOCKER_IMAGE:$DOCKER_TAG' already exists, building aborted.'
-    echo
+else
+    echo 'Unknown response from docker manifest inspect:'
+    echo $MANIFEST_RESULT
+    exit 1
 fi
 
 docker pull $DOCKER_IMAGE:$DOCKER_TAG
